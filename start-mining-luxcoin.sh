1029ad06282a5400dbca6ba8ffa4c928387447ea
seefong <90110420+seefong@users.noreply.github.com>
2021-09-04 06:44:26 -1200

    Add files via upload

---
+++ start-mining-luxcoin.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+./SRBMiner-MULTI --disable-gpu --algorithm rx2 --pool rx2.asia.mine.zergpool.com:8335 --wallet bitcoincash:qzam249s4ywp70vw4zchsqng0wvawa66pumn89vd7k -p c=BCH,mc=LUX,ID=Rig1
